5ab0d09eca8742757c3cf31b7d75ed895a5519d0
dugksl23 <dugksl23@gmail.com>
2024-04-29 16:36:38 +0900

    addForm 서버측 검증 로직 개발 중

--- src/main/java/hello/itemservice/web/validation/ValidationItemControllerV1.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemControllerV1.java
@@ -1,21 +1,29 @@
 package hello.itemservice.web.validation;
 
+import com.google.common.base.Preconditions;
+import com.google.common.base.Strings;
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.flogger.Flogger;
+import lombok.extern.slf4j.Slf4j;
+import org.checkerframework.common.value.qual.IntVal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.util.List;
+import java.util.*;
 
 @Controller
 @RequestMapping("/validation/v1/items")
 @RequiredArgsConstructor
-public class  ValidationItemControllerV1 {
+@Slf4j
+public class ValidationItemControllerV1 {
 
     private final ItemRepository itemRepository;
+
 
     @GetMapping
     public String items(Model model) {
@@ -38,7 +46,45 @@ public class  ValidationItemControllerV1 {
     }
 
     @PostMapping("/add")
-    public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
+    public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes, Model model) {
+
+        //검증 오류 결과를 보관
+        Map<String, String> errors = new HashMap<>();
+
+        // 필드 검증 로직
+        if (!StringUtils.hasText(item.getItemName()) || Strings.isNullOrEmpty(item.getItemName())) {
+            log.info("itemName : {}", item.getItemName());
+            errors.put("itemName", "상품 이름은 필수입니다. ");
+        }
+        if (item.getPrice() == null || item.getPrice() < 999 || item.getPrice() > 1000000) {
+            log.info("price : {}", item.getPrice());
+            errors.put("price", "가격은 1,000 ~ 1,000,000 까지 허용합니다.");
+        }
+        if (Optional.ofNullable(item.getQuantity()).isEmpty() || item.getQuantity() == 0 || item.getQuantity() > 9999) {
+            log.info("Quantity : {}", item.getQuantity());
+            errors.put("quantity", "수량은 최대 9,999 까지 허용합니다.");
+        }
+
+        // 특정 필드가 아닌 복합 룰 검증
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            // 기존의 and　→ or 문법으로 교
+            int orderItemPrice = item.getPrice() * item.getQuantity();
+            ;
+            if (orderItemPrice < 10000) {
+                errors.put("limitOrderPriceError", "최소 주문 금액은 10,000 이상입니다.");
+                errors.put("currentOrderPrice", "현재 주문 금액 : " + orderItemPrice);
+            }
+        }
+
+
+        // 검증 실패시 다시 입력 Form 으로 Redirect
+        if (!errors.isEmpty()) {
+            log.info("errors : {}", errors.toString());
+            model.addAttribute("errors", errors);
+            return "validation/v1/addForm"; // 검증 실패시 입력 Page로 다시 보내기.
+        }
+
+        // 성공 로직
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
